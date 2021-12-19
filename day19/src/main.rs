10f0c723f509b9c66823332043fd1cc4669a38c4
Nils Alex <nils@famalex.de>
2021-12-19 10:50:47 +0100

    day 19 part 2

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -33,7 +33,7 @@ fn rotate(pos: &mut (i32, i32, i32), rots: &mut (u8, u8, u8)) {
 fn transform_to(
     probe1: &[(i32, i32, i32)],
     probe2: &[(i32, i32, i32)],
-) -> Option<Vec<(i32, i32, i32)>> {
+) -> Option<((i32, i32, i32), Vec<(i32, i32, i32)>)> {
     for rotx in 0_u8..=3 {
         for roty in 0_u8..=3 {
             for rotz in 0_u8..=3 {
@@ -53,14 +53,15 @@ fn transform_to(
                         if let Some(count) = diffs.get_mut(&diff) {
                             *count += 1;
                             if *count == 12 {
-                                return Some(
+                                return Some((
+                                    diff,
                                     probe2_
                                         .iter()
                                         .map(|(x__, y__, z__)| {
                                             (x__ + diff.0, y__ + diff.1, z__ + diff.2)
                                         })
                                         .collect(),
-                                );
+                                ));
                             }
                         } else {
                             diffs.insert(diff, 1);
@@ -76,12 +77,12 @@ fn transform_to(
 
 fn part1() -> usize {
     let file = File::open("day19/input.txt").unwrap();
-    let mut lines = io::BufReader::new(file).lines().flatten();
+    let lines = io::BufReader::new(file).lines().flatten();
 
     let mut maps: Vec<Vec<(i32, i32, i32)>> = vec![];
     let mut cur_map: Vec<(i32, i32, i32)> = vec![];
 
-    while let Some(line) = lines.next() {
+    for line in lines {
         if line.is_empty() {
             maps.push(cur_map.clone());
             continue;
@@ -112,12 +113,10 @@ fn part1() -> usize {
     while !remaining_maps.is_empty() {
         let mut new_result_maps: Vec<Vec<(i32, i32, i32)>> = vec![];
         let mut new_remaining_maps: Vec<Vec<(i32, i32, i32)>> = vec![];
-        println!("{}", remaining_maps.len());
 
         'outer: for map2 in remaining_maps.iter() {
             for map1 in result_maps.iter() {
-                if let Some(map2_) = transform_to(map1, map2) {
-                    println!("overlap!");
+                if let Some((_, map2_)) = transform_to(map1, map2) {
                     new_result_maps.push(map2_);
                     continue 'outer;
                 }
@@ -137,8 +136,6 @@ fn part1() -> usize {
         }
     }
 
-    println!("{}", result_maps.len());
-
     result_set.len()
 }
 
@@ -146,7 +143,69 @@ fn part2() -> usize {
     let file = File::open("day19/input.txt").unwrap();
     let lines = io::BufReader::new(file).lines().flatten();
 
-    0
+    let mut maps: Vec<Vec<(i32, i32, i32)>> = vec![];
+    let mut cur_map: Vec<(i32, i32, i32)> = vec![];
+
+    for line in lines {
+        if line.is_empty() {
+            maps.push(cur_map.clone());
+            continue;
+        }
+
+        if line.starts_with("---") {
+            cur_map = vec![];
+            continue;
+        }
+
+        let pos = match &line
+            .split(',')
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>()[0..]
+        {
+            &[x_, y_, z_, ..] => (x_, y_, z_),
+            _ => unreachable!(),
+        };
+
+        cur_map.push(pos);
+    }
+
+    maps.push(cur_map.clone());
+
+    let mut result_maps: Vec<Vec<(i32, i32, i32)>> = vec![maps[0].clone()];
+    let mut remaining_maps: Vec<Vec<(i32, i32, i32)>> = maps.iter().skip(1).cloned().collect();
+    let mut diffs: Vec<(i32, i32, i32)> = vec![(0, 0, 0)];
+
+    while !remaining_maps.is_empty() {
+        let mut new_result_maps: Vec<Vec<(i32, i32, i32)>> = vec![];
+        let mut new_remaining_maps: Vec<Vec<(i32, i32, i32)>> = vec![];
+
+        'outer: for map2 in remaining_maps.iter() {
+            for map1 in result_maps.iter() {
+                if let Some((diff, map2_)) = transform_to(map1, map2) {
+                    new_result_maps.push(map2_);
+                    diffs.push(diff);
+                    continue 'outer;
+                }
+            }
+            new_remaining_maps.push(map2.clone());
+        }
+
+        result_maps.append(&mut new_result_maps);
+        remaining_maps = new_remaining_maps;
+    }
+
+    let mut max_diff: usize = 0;
+
+    for (dx1, dy1, dz1) in diffs.iter() {
+        for (dx2, dy2, dz2) in diffs.iter() {
+            max_diff = std::cmp::max(
+                max_diff,
+                ((dx1 - dx2).abs() + (dy1 - dy2).abs() + (dz1 - dz2).abs()) as usize,
+            );
+        }
+    }
+
+    max_diff
 }
 
 fn main() {
